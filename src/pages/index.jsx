354284869d4dd6f677839a09b644cc612c7bc65f
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-01-07 19:05:29 +0900

    試しにクリックイベントを作成

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,34 @@
 // Next.js
 import Head from 'next/head'
 
+// React
+import { useCallback } from 'react';
+
 // components
 import { Header } from 'src/components/Header'
 import { Main } from 'src/components/Main'
 
+
 export default function Home() {
+  const foo = 1;
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a
+        href="/about"
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
       <Main link={"pages/index.js"} />
     </>
   )
